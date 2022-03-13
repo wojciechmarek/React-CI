44ce12fb05529179a103c77e9f0c6f1b547fbb10
Wojciech Marek <marek.wojciech@outlook.com>
2022-03-13 23:29:04 +0100

    Remove existing CI file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("just return true", () => {
+  const a = 1;
+  const b = 4;
+  const sum = a + b;
+  expect(sum).toBe(5);
 });
